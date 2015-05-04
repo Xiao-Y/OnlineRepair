b2bd6d3f4aff8e3cfe2e14937d4deb34e9e010df
Xiao-Y <lyongtao123@126.com>
2015-05-04 15:16:12 +0800

    修改统计管理中有统计以两种方式显示

--- WebContent/WEB-INF/page/CountMag/count.jsp
+++ WebContent/WEB-INF/page/CountMag/count.jsp
@@ -1,12 +1,24 @@
 <%@ page language="java" pageEncoding="UTF-8" contentType="text/html; charset=utf-8"%>
+<%@ taglib prefix="s"  uri="/struts-tags"%>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>统计报表</title>
-
+<script type="text/javascript">
+//链接跳转
+function link(href){
+	window.location.href=href;
+}
+</script>
 </head>
 
 <body  style="background-color:#F5FAFE;">
+	<s:if test="%{#request.flag == 1 || #request.flag == ''}">
+		<input style="font-size:12px; color:black; height=20;width=80" type="button" value="切换条形图" onclick="link('${pageContext.request.contextPath }${path}?flag=0')">
+	</s:if>
+	<s:else>
+		<input style="font-size:12px; color:black; height=20;width=80" type="button" value="切换饼状图" onclick="link('${pageContext.request.contextPath }${path}?flag=1')">
+	</s:else>
 	<div align="center">
 		<img style="width: 700px; height: 500px" src="${pageContext.request.contextPath}/chart/${picName}" border="0">
 	</div>
--- src/com/xiaoy/count/web/action/CountAction.java
+++ src/com/xiaoy/count/web/action/CountAction.java
@@ -20,8 +20,8 @@ public class CountAction extends BaseAction
 	public final static String MENU_MODE = "【统计管理】";
 	@Resource
 	private CountService countService;
-	
-	//日志
+
+	// 日志
 	@Resource
 	private LogService logService;
 
@@ -33,24 +33,49 @@ public class CountAction extends BaseAction
 	 */
 	public String userSexCount() throws IOException
 	{
+		String flag = request.getParameter("flag");
+
 		Map<String, Double> map = countService.userSexCount();
 		Reports rp = new Reports();
 		String title = "用户性别比例";
-		DefaultPieDataset dataset = new DefaultPieDataset();
-		if (map != null && map.size() > 0)
+		String picName = "";
+		if ("1".equals(flag))
 		{
-			for (Entry<String, Double> entry : map.entrySet())
+			DefaultPieDataset dataset = new DefaultPieDataset();
+			if (map != null && map.size() > 0)
 			{
-				dataset.setValue(entry.getKey(), entry.getValue());
+				for (Entry<String, Double> entry : map.entrySet())
+				{
+					dataset.setValue(entry.getKey(), entry.getValue());
+				}
 			}
+
+			picName = "chartPicUserSexCount.jpeg";
+
+			rp.pie(dataset, title, request, picName);
+		} else
+		{
+			String categoryAxisLabel = "用户性别";
+			String valueAxisLabel = "数量";
+			DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+			// value 值, rowKey 行值, columnKey 列值
+			if (map != null && map.size() > 0)
+			{
+				for (Entry<String, Double> entry : map.entrySet())
+				{
+					dataset.setValue(entry.getValue(), "用户性别", entry.getKey());
+				}
+			}
+
+			picName = "chartBarUserSexCount.jpeg";
+
+			rp.bar(dataset, title, categoryAxisLabel, valueAxisLabel, request, picName);
 		}
 
-		String picName = "chartPicUserSexCount.jpeg";
-
-		rp.pie(dataset, title, request, picName);
-
 		request.setAttribute("picName", picName);
-
+		request.setAttribute("path", "/CountMag/countAction_userSexCount.action");
+		request.setAttribute("flag", flag);
+		
 		logService.saveLog(request, MENU_MODE, "用户性别统计");
 		return "success";
 	}
@@ -63,26 +88,49 @@ public class CountAction extends BaseAction
 	 */
 	public String userTypeCount() throws IOException
 	{
+		String flag = request.getParameter("flag");
+
 		Map<String, Double> map = countService.userTypeCount();
 		Reports rp = new Reports();
 		String title = "用户维护类型";
-		String categoryAxisLabel = "维护类型";
-		String valueAxisLabel = "数量";
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		// value 值, rowKey 行值, columnKey 列值
-		if (map != null && map.size() > 0)
+		String picName = "";
+		if ("1".equals(flag))
 		{
-			for (Entry<String, Double> entry : map.entrySet())
+			DefaultPieDataset dataset = new DefaultPieDataset();
+			if (map != null && map.size() > 0)
 			{
-				dataset.setValue(entry.getValue(), "维护类型", entry.getKey());
+				for (Entry<String, Double> entry : map.entrySet())
+				{
+					dataset.setValue(entry.getKey(), entry.getValue());
+				}
 			}
+
+			picName = "chartPicUserTypeCount.jpeg";
+
+			rp.pie(dataset, title, request, picName);
+		} else
+		{
+			String categoryAxisLabel = "维护类型";
+			String valueAxisLabel = "数量";
+			DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+			// value 值, rowKey 行值, columnKey 列值
+			if (map != null && map.size() > 0)
+			{
+				for (Entry<String, Double> entry : map.entrySet())
+				{
+					dataset.setValue(entry.getValue(), "维护类型", entry.getKey());
+				}
+			}
+
+			picName = "chartBarUserTypeCount.jpeg";
+
+			rp.bar(dataset, title, categoryAxisLabel, valueAxisLabel, request, picName);
 		}
 
-		String picName = "chartBaruserTypeCount.jpeg";
-
-		rp.bar(dataset, title, categoryAxisLabel, valueAxisLabel, request, picName);
-
 		request.setAttribute("picName", picName);
+		request.setAttribute("path", "/CountMag/countAction_userTypeCount.action");
+		request.setAttribute("flag", flag);
+		
 		logService.saveLog(request, MENU_MODE, "用户的维护类型");
 		return "success";
 	}
@@ -95,26 +143,47 @@ public class CountAction extends BaseAction
 	 */
 	public String deviceBreakdownCount() throws IOException
 	{
+		String flag = request.getParameter("flag");
+
 		Map<String, Double> map = countService.deviceBreakdownCount();
 		Reports rp = new Reports();
 		String title = "设备故障次数统计";
-		String categoryAxisLabel = "设备故障次数";
-		String valueAxisLabel = "次数";
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		// value 值, rowKey 行值, columnKey 列值
-		if (map != null && map.size() > 0)
+		String picName = "";
+		if ("1".equals(flag))
 		{
-			for (Entry<String, Double> entry : map.entrySet())
+			DefaultPieDataset dataset = new DefaultPieDataset();
+			if (map != null && map.size() > 0)
 			{
-				dataset.setValue(entry.getValue(), "设备故障次数", entry.getKey());
+				for (Entry<String, Double> entry : map.entrySet())
+				{
+					dataset.setValue(entry.getKey(), entry.getValue());
+				}
 			}
+
+			picName = "chartPicAkdownCount.jpeg";
+
+			rp.pie(dataset, title, request, picName);
+		} else
+		{
+			String categoryAxisLabel = "设备故障次数";
+			String valueAxisLabel = "次数";
+			DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+			// value 值, rowKey 行值, columnKey 列值
+			if (map != null && map.size() > 0)
+			{
+				for (Entry<String, Double> entry : map.entrySet())
+				{
+					dataset.setValue(entry.getValue(), "设备故障次数", entry.getKey());
+				}
+			}
+
+			picName = "deviceBreakdownCount.jpeg";
+
+			rp.bar(dataset, title, categoryAxisLabel, valueAxisLabel, request, picName);
 		}
-
-		String picName = "deviceBreakdownCount.jpeg";
-
-		rp.bar(dataset, title, categoryAxisLabel, valueAxisLabel, request, picName);
-
 		request.setAttribute("picName", picName);
+		request.setAttribute("path", "/CountMag/countAction_deviceBreakdownCount.action");
+		request.setAttribute("flag", flag);
 		
 		logService.saveLog(request, MENU_MODE, "设备故障次数统计");
 		return "success";
@@ -122,31 +191,55 @@ public class CountAction extends BaseAction
 
 	/**
 	 * 设备数量统计
+	 * 
 	 * @return
 	 * @throws IOException
 	 */
 	public String deviceSum() throws IOException
 	{
+		String flag = request.getParameter("flag");
+
 		Map<String, Double> map = countService.deviceSum();
 		Reports rp = new Reports();
 		String title = "设备数量统计";
-		String categoryAxisLabel = "设备数量";
-		String valueAxisLabel = "数量";
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		// value 值, rowKey 行值, columnKey 列值
-		if (map != null && map.size() > 0)
+		String picName = "";
+		if ("1".equals(flag))
 		{
-			for (Entry<String, Double> entry : map.entrySet())
+			DefaultPieDataset dataset = new DefaultPieDataset();
+			if (map != null && map.size() > 0)
 			{
-				dataset.setValue(entry.getValue(), "设备数量", entry.getKey());
+				for (Entry<String, Double> entry : map.entrySet())
+				{
+					dataset.setValue(entry.getKey(), entry.getValue());
+				}
 			}
+
+			picName = "chartPicDeviceSum.jpeg";
+
+			rp.pie(dataset, title, request, picName);
+		} else
+		{
+			String categoryAxisLabel = "设备数量";
+			String valueAxisLabel = "数量";
+			DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+			// value 值, rowKey 行值, columnKey 列值
+			if (map != null && map.size() > 0)
+			{
+				for (Entry<String, Double> entry : map.entrySet())
+				{
+					dataset.setValue(entry.getValue(), "设备数量", entry.getKey());
+				}
+			}
+
+			picName = "chartBerDeviceSum.jpeg";
+
+			rp.bar(dataset, title, categoryAxisLabel, valueAxisLabel, request, picName);
 		}
-
-		String picName = "deviceSum.jpeg";
-
-		rp.bar(dataset, title, categoryAxisLabel, valueAxisLabel, request, picName);
-
+		
 		request.setAttribute("picName", picName);
+		request.setAttribute("path", "/CountMag/countAction_deviceSum.action");
+		request.setAttribute("flag", flag);
+		
 		logService.saveLog(request, MENU_MODE, "设备数量统计");
 		return "success";
 	}
